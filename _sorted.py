183524e7176c2ac4146826cabbd4df4cb79b5b1c
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-02-07 13:50:40 -0800

    Write _sorted docstring

---
+++ _sorted.py
@@ -0,0 +1,9 @@
+def _sorted(list_of_integers):
+    """
+    Determine if a list of integers is sorted in non-decreasing order.
+
+    :param list_of_integers: a list containing only integers, may be empty
+    :precondition: list_of_integers must either be empty or contain only integers
+    :postcondition: determines whether a list of integers is sorted or not
+    :return: True if the list is sorted, else False
+    """
